2e21a2041c051b0ac6959bf4719d91d03f474cf7
Kshitiz Sharma <88051684+Kshitiz-Sharma26@users.noreply.github.com>
2023-07-05 10:55:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
+
 
 const connect = require("./database");
 connect();
@@ -10,10 +12,11 @@ const User = require("./models/userData.js");
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors);
 const route = require('./routes.js');
 app.use('/', route);
 
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("listening")
 })
